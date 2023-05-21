f2b0387923f3f76b999672b59b7893db67cb672c
Joseph Sullivan <josephsullivan256@gmail.com>
2023-05-21 15:58:52 -0700

    got three-d example running

--- README.md
+++ README.md
@@ -13,6 +13,32 @@ Tool for computing topological invariants of level sets of submersions.
 - Create a Linear/Abstract Algebra library to handle relevant computations discussed above.
 - Have an editor for building submersions then compute triangulations/homology.
 
+# Building
+
+## Native
+
+Run
+
+`cargo run`
+
+and you're good to go.
+
+## For Web
+
+You'll need
+- [wasm-pack](https://rustwasm.github.io/wasm-pack/)
+- a web server (I'm using [http-server](https://www.npmjs.com/package/http-server))
+
+First build the project by running
+
+`wasm-pack build --target web --out-name web --out-dir ./pkg`
+
+Then, start the server by running
+
+`npx http-server`
+
+and go to [http://localhost:8080/index.html](http://localhost:8080/index.html).
+
 # License
 
 Licensed under the MIT license (LICENSE-MIT or http://opensource.org/licenses/MIT).
---
+++ pkg/web_bg.wasm.d.ts
@@ -0,0 +1,15 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function start(): void;
+export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number): number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hd264c062763c1777(a: number, b: number, c: number): void;
+export function _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hc9c9e90bfd400fe6(a: number, b: number): void;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h602ae2d4a1274d1d(a: number, b: number, c: number): void;
+export function _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hf5bdc6b9de08882b(a: number, b: number): void;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h5e128ea4d6ca3cf9(a: number, b: number, c: number): void;
+export function __wbindgen_free(a: number, b: number): void;
+export function __wbindgen_exn_store(a: number): void;
+export function __wbindgen_start(): void;
